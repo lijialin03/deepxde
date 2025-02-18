d398bed3a61119871f88fd47a871c67f008b767d
lijialin03 <lijialin03@baidu.com>
2025-02-18 11:21:26 +0000

    Backend paddle: allow enable prim to accelerate running

--- deepxde/backend/paddle/__init__.py
+++ deepxde/backend/paddle/__init__.py
@@ -1,1 +1,13 @@
 from .tensor import *  # pylint: disable=redefined-builtin
+import os
+
+# enable prim if specified
+enable_prim_value = os.getenv("PRIM")
+enable_prim = enable_prim_value.lower() in ['1', 'true', 'yes', 'on'] if enable_prim_value else False
+if enable_prim:
+    # Mostly for compiler running with dy2st.
+    from paddle.framework import core
+
+    core.set_prim_eager_enabled(True)
+    core._set_prim_all_enabled(True)
+    print("Prim mode is enabled.")
